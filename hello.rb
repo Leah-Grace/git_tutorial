c6d98cc98b3d9b2342391c5090951c68f4ef8b7c
Leah_Grace <leahgdg@hotmail.com>
2022-02-26 16:46:05 -0500

    Moved hello.rb to lib

--- hello.rb
+++
@@ -1,8 +0,0 @@
-# Default is World
-# Author: Jim Weirich (jim@somewhere.com))
-
-name = ARGV.first || "World"
-
-puts "Hello, #{name}!"
-
-"Hello, #{ARGV.first}!"
--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,12 @@
 puts Hello, World
 # Default is World
 # Author: Leah Grace (grace@mail.com)
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com))
+
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+"Hello, #{ARGV.first}!"
+
